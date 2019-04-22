658b9eae547c480dd0eff5ca05ce1fb538ac2616
Daniel Mauser <dmauser@hotmail.com>
2019-04-22 18:32:23 -0500

    Script changes

--- scripts/configureopnsense.sh
+++ scripts/configureopnsense.sh
@@ -1,17 +1,11 @@
 #!/bin/sh
-
-ARTIFACTSLOCATION=${1}
-ALTHOSTNAME=${2}
-
-sudo fetch ${ARTIFACTSLOCATION}scripts/config.xml
-
-sed -i "" "s/ZZZALTHOSTNAMEZZZ/$ALTHOSTNAME/g" config.xml
+sudo fetch https://raw.githubusercontent.com/dmauser/opnazure/master/scripts/config.xml
 
 sudo mkdir /conf
 sudo cp config.xml /conf/config.xml
 
 sudo env ASSUME_ALWAYS_YES=YES pkg install ca_root_nss
-sudo fetch https://raw.githubusercontent.com/opn/update/bootstrap/opnsense-bootstrap.sh
+sudo fetch https://raw.githubusercontent.com/opnsense/update/master/bootstrap/opnsense-bootstrap.sh
 
 sed -i "" "s/reboot/shutdown -r +1/g" opnsense-bootstrap.sh
 
