18285d3a213ec02a2ac0e1fa4685d35a331e217f
Daniel Mauser <dmauser@hotmail.com>
2019-04-22 20:03:23 -0500

    shell script adjustments

--- scripts/configureopnsense.sh
+++ scripts/configureopnsense.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-sudo fetch https://raw.githubusercontent.com/dmauser/opnazure/master/scripts/config.xml
+fetch https://raw.githubusercontent.com/dmauser/opnazure/master/scripts/config.xml
 
-sudo mkdir /conf
-sudo cp config.xml /conf/config.xml
+mkdir /conf
+cp config.xml /conf/config.xml
 
-sudo env ASSUME_ALWAYS_YES=YES pkg install ca_root_nss
-sudo fetch https://raw.githubusercontent.com/opnsense/update/master/bootstrap/opnsense-bootstrap.sh
+env ASSUME_ALWAYS_YES=YES pkg install ca_root_nss
+fetch https://raw.githubusercontent.com/opnsense/update/master/bootstrap/opnsense-bootstrap.sh
 
 sed -i "" "s/reboot/shutdown -r +1/g" opnsense-bootstrap.sh
 
-sudo sh ./opnsense-bootstrap.sh -y
+sh ./opnsense-bootstrap.sh -y
