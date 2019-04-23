ee89200e2da6c3c985ae7b7ea32861727eda60d2
Daniel Mauser <dmauser@hotmail.com>
2019-04-23 09:55:59 -0500

    Adjustments on custom script

--- scripts/configureopnsense.sh
+++ scripts/configureopnsense.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 fetch https://raw.githubusercontent.com/dmauser/opnazure/master/scripts/config.xml
-
-mkdir /conf
-cp config.xml /conf/config.xml
+cp config.xml /usr/local/etc/config.xml
 
 env ASSUME_ALWAYS_YES=YES pkg install ca_root_nss
+
+#Permit Root Remote Login
 fetch https://raw.githubusercontent.com/opnsense/update/master/bootstrap/opnsense-bootstrap.sh
+sed -i 's/PermitRootLogin yes/PermitRootLogin no/g' /etc/ssh/sshd_config
 
+#OPNSense
 sed -i "" "s/reboot/shutdown -r +1/g" opnsense-bootstrap.sh
-
 sh ./opnsense-bootstrap.sh -y
