8f0ae94e789113214e38924736f4ee0b00a9f85e
Daniel Mauser <dmauser@hotmail.com>
2021-04-02 15:18:49 -0500

    changed to master

--- scripts/configureopnsense.sh
+++ scripts/configureopnsense.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #OPNSense default configuration template
-fetch https://raw.githubusercontent.com/dmauser/opnazure/dev/scripts/config.xml
+fetch https://raw.githubusercontent.com/dmauser/opnazure/master/scripts/config.xml
 cp config.xml /usr/local/etc/config.xml
 
 # 1. Package to get root certificate bundle from the Mozilla Project (FreeBSD)
