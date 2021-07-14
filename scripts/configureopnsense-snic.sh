6ba30a97035d49d469b3377a9e27ba7ebc626651
Daniel Mauser <dmauser@hotmail.com>
2021-07-14 14:03:50 -0500

    update
    fixed bootstrap with correct command

--- scripts/configureopnsense-snic.sh
+++ scripts/configureopnsense-snic.sh
@@ -13,7 +13,7 @@ sed -i "" 's/#PermitRootLogin no/PermitRootLogin yes/' /etc/ssh/sshd_config
 
 #OPNSense
 sed -i "" "s/reboot/shutdown -r +1/g" opnsense-bootstrap.sh.in
-sh ./opnsense-bootstrap.sh.in
+sh ./opnsense-bootstrap.sh.in -y -r "21.1"
 #Adds support to LB probe from IP 168.63.129.16
 fetch https://raw.githubusercontent.com/dmauser/opnazure/master/scripts/lb-conf.sh
 sh ./lb-conf.sh
